0bf9cae326981a20437d8dde9bff400eb6f7eaec
AnaisAna <anais.fradin@laposte.net>
2019-01-23 21:57:50 +0100

    exercice4.html

---
+++ script.js
@@ -0,0 +1,6 @@
+function wipeoff(){
+  //value="" ca efface les données des champs du formulaire//
+  document.getElementById('lastname').value="";
+  document.getElementById('firstname').value="";
+  document.getElementById('city').value="";
+}
